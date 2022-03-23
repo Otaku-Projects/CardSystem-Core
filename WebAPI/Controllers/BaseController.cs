292295a286717bce8107b307497cfd509288719f
KeithPoonNS <89375575+KeithPoonNS@users.noreply.github.com>
2022-03-23 16:05:01 +0800

    add: set default produces to application/json

--- WebAPI/Controllers/BaseController.cs
+++ WebAPI/Controllers/BaseController.cs
@@ -7,6 +7,7 @@ namespace WebAPI.Controllers
     //[AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class BaseController : ControllerBase
     {
     }
